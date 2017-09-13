3ab40077649dfde1ee82d5d8a341c4f2e6e18a66
OlegRyz <aleh.ryzhankou@gmail.com>
2017-09-13 22:48:57 +0200

    Solution for Koan N34 without using lazy() function

--- src/iv_properties/n34DelegatesExamples.kt
+++ src/iv_properties/n34DelegatesExamples.kt
@@ -2,9 +2,22 @@ package iv_properties
 
 import util.TODO
 import util.doc34
+import kotlin.reflect.KProperty
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by lazy(initializer)
+    val lazyValue: Int by MyDelegate(initializer)
+}
+
+class MyDelegate(val initializer: () -> Int) {
+    var isInitialized = false
+    var value = 0
+    operator fun getValue(motherClass: LazyPropertyUsingDelegates?, property: KProperty<*>): Int {
+        if (!isInitialized) {
+            value = initializer()
+            isInitialized = true
+        }
+        return value
+    }
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
